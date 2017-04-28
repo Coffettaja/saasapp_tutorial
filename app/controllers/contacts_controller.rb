a00802d68ad86a28b1edf49c92b86412731fb336
Joonas K <coffettaja@gmail.com>
2017-04-28 14:52:24 +0000

    Added some comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,21 @@
 class ContactsController < ApplicationController
+    #Show new contact form
+    #TODO: Don't show completely new form when error
     def new
         @contact = Contact.new
     end
     
+    # POST request /contacts
     def create
+        #Mass assignment of form fields
        @contact = Contact.new(contact_params) 
        
        if @contact.save
            name = params[:contact][:name]
            email = params[:contact][:email]
            body = params[:contact][:comments]
+           
+           #Plug variables into Contact Mailer email method and send the mail
            ContactMailer.contact_email(name, email, body).deliver
            flash[:success] = "Message sent."
            redirect_to contact_us_path
@@ -20,6 +26,8 @@ class ContactsController < ApplicationController
     end
     
     private 
+        # To collect data from form, strong parameters have to be used
+        # and form fields have to be whitelisted
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
         end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    #TODO: Better validations, at least for email
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
