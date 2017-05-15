5438d217b2a846bf57a35b6e78f0f0825dfaf2be
Joonas K <coffettaja@gmail.com>
2017-05-15 18:47:30 +0000

    User profile nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  
+  #Nested route for user profiles; if there were many, use plurals
+  resources :users do
+    resource :profile
+  end
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: :contact_us
