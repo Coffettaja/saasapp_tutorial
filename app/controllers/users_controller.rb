fb610b2a6ffc23d6ee50e5d4b3094f15d34135c8
Joonas K <coffettaja@gmail.com>
2017-05-26 15:46:38 +0000

    Users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
    # Run this devise method before any action is run, is the user logged in?
    before_action :authenticate_user!
    
+   def index
+      
+   end
+   
    # GET to /users/:id
    # Use 'show' to show individual resource
    def show
